1b5cf15c0578c9e02b25654fdc0a885b553e83ba
Monir-dev <echomonir@gmail.com>
2018-10-30 17:50:44 +0600

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # rtsp-stream
-
-Kurulum 
+ 
 npm i 
 node app.js 
--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 Stream = require('node-rtsp-stream');
 stream = new Stream({
     name: 'name',
-    streamUrl: 'rtsp://192.168.2.26:554/av0_0',
+    streamUrl: 'rtsp://admin:1qaz2wsx@172.18.77.53/Streaming/Channels/101/',
     wsPort: 9999
 });
-
-
+stream = new Stream({
+    name: 'name',
+    streamUrl: 'rtsp://admin:1qaz2wsx@172.18.77.53/Streaming/Channels/101/',
+    wsPort: 9990
+});
+// rtsp://admin:1qaz2wsx@172.18.77.53/Streaming/Channels/101/
+// rtsp://192.168.2.26:554/av0_0
 
 
 
